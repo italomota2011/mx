78b6dd2bdc1c7426b9004b4c047028aa0a9e7684
prorally <italo-mota@hotmail.com>
2026-02-23 13:19:41 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,40 @@
-const CACHE_NAME = 'mx-timer-v2';
-const assets = [
+// sw.js
+const CACHE_NAME = 'mx-timer-v1';
+const ASSETS = [
   './',
   './index.html',
   './manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://raw.githubusercontent.com/italomota2011/mx/refs/heads/main/logo.png',
   'https://github.com/italomota2011/mx/blob/main/icon.jpg?raw=true'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(assets);
+      return cache.addAll(ASSETS);
     })
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+  self.claim();
 });
 
 self.addEventListener('fetch', (event) => {
+  // NÃO cacheia chamadas do Firebase (Firestore/Auth)
+  if (event.request.url.includes('googleapis.com') || event.request.url.includes('firebasejs')) {
+    return;
+  }
+
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
